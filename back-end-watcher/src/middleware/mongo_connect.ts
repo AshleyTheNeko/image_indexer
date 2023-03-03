f5212e547256e7d44f82fe632b4261614dc6a0d9
Ashley <ashley.damiens@epitech.eu>
2023-03-03 07:43:46 +0100

    [RM] removed watcher

--- back-end-watcher/src/middleware/mongo_connect.ts
+++
@@ -1,6 +0,0 @@
-import { MongoClient, MongoClientOptions } from "mongodb";
-
-const uri = `mongodb://${process.env.MONGO_HOST}:27017/typesense`;
-const client = new MongoClient(uri);
-
-export default client;
